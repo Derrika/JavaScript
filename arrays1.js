38e852b89c30f2d8b8929df23cef6683ad9cbc40
Derrika <coley.derrika79@gmail.com>
2022-05-01 20:57:01 -0500

    Added arrays1.js

---
+++ arrays1.js
@@ -0,0 +1,13 @@
+//Array
+const users = ['Matt', 'Matt', 'Nancy', 'Adam', 'Nancy'];
+console.log({users});
+
+//unique values
+const unique = Array.from(new Set(users));
+console.log({unique});
+
+
+//JavaScript Spread Operator
+
+const newFood = ['🍉', '🍓', '🍇', '🍒'];
+const yourFood = ['🍅', '🍕'];
