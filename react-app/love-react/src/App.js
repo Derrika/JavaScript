e4d2d23084bd691e11de1c3accc2d1ac4a841d8a
Derrika <coley.derrika79@gmail.com>
2022-05-03 20:28:32 -0500

    made chages

--- react-app/love-react/src/App.js
+++ react-app/love-react/src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
           Hello Welcome!!
         </p>
+        <ul>
+          {props.lakes.map(lake => (
+            <li>{lake}</li>
+          ))}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
--- react-app/love-react/src/index.js
+++ react-app/love-react/src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import App from './App'
+import App from './App'
 import './index.css';
 
 
 const lakeList = ["Echo Lake", "Maud Lake", "Cascade Lake"];
-
-function App(props) {
-  return (
-   
-    <ul>
-      {props.lakes.map(lake => (
-        <li>{lake}</li>
-      ))}
-    </ul>
-  )
-}
 
 ReactDOM.render(
   <App lake={lakeList}/>, 
